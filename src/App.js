881db48ab6447d162143c57809fe0f48ae8be2f4
Michell Aagaard Dranig <tiamisse@gmail.com>
2021-01-16 22:15:35 +0100

    passing reference and method handling

--- src/App.js
+++ src/App.js
@@ -21,11 +21,11 @@ class App extends Component {
     otherState: 'some other value',
   };
 
-  switchNameHandler = () => {
+  switchNameHandler = newName => {
     this.setState({
       persons: [
         {
-          name: 'Maximilian',
+          name: newName,
           age: 28,
         },
         {
@@ -40,14 +40,32 @@ class App extends Component {
     });
   };
 
+  // if you want to use a function fx when clicking each paragraph in person you can make a reference from this component by making
+  // using a prop called click and then the name of the reference (in this case the switchNameHandler)
+  // that prop will of course need to be on the p paragraph in the person component
+  // so you can pass a method as props so that you can call a method which might change state in another component
+  // that doesn't or shouldn't have direct access to the state
+
+  // when using arrow functions it explicitly returns a 'return' keyword (so whatever the function should return)
+
+  // onClick={() => this.switchNameHandler()} = when this function is called then call the switchNameHandler function
+  // if you don't have the () => in front of this.switchNameHandler there shouldn't be () after the switch function because the it would
+  // be called automatically when rendered.
+  // but when we have an arrow function inside the onClick and that function will only execute when clicked on so that means the nameHandler
+  // should be executed immideately as the onClick is called
+  // it can be inefficient though - react can sometimes rerender things too much
+
   render() {
     return (
       <div className='App'>
         <h1>Hi I'm a React App</h1>
         <p>This is really working!</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <button onClick={() => this.switchNameHandler('Maximilian!!')}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Max!')}>
           My hobbies: Racing
         </Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
